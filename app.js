611fcfab02faced19a28c8ab59d023ee5fd27103
yanmoenaing118 <yan.webdev.1234@gmail.com>
2020-09-29 14:57:59 +0630

    delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-let iframe = document.getElementById("iframe");
-let code = document.getElementById("code");
-code.addEventListener("input", (e) => {
-  let iDoc = iframe.contentDocument;
-  iDoc.body.innerHTML = e.target.value;
-});
